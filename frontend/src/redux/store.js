58d2aac72d434dd99b95be0e445d1d57515d58f8
lostsoul410 <lostsoul410200@gmail.com>
2023-12-30 15:17:17 +0530

    font

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -17,3 +17,5 @@ const store = configureStore({
 export default store;
 
 export const server = "https://burgermern.vercel.app/api/v1";
+//@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap');
+// font-family: 'Poppins', sans-serif;
